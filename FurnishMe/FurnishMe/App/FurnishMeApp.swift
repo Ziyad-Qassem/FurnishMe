397ce3d7894866ac925a2f09d6c7aa1273e7b9a5
Ziyad-Qassem <Qassemziyad007@gmail.com>
2025-01-06 06:24:55 +0200

    project skeleton finished

---
+++ FurnishMe/FurnishMe/App/FurnishMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnishMeApp.swift
+//  FurnishMe
+//
+//  Created by Ziyad Qassem on 06/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FurnishMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
